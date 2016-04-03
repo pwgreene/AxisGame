dc2a65a624382e095b57c737f6e1aaec21df9887
Johnathon Root <johnathon.root@gmail.com>
2016-04-03 16:06:25 -0400

    EnemyManager and Core created
    Enemies spawn off screen and head towards the Core, damaging it until it
    despawns itself.

---
+++ PC Master Client/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public float speed;
+    public int damage;
+
+    Vector3 corePosition;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        try
+        {
+            corePosition = GameObject.FindGameObjectWithTag("Core").transform.position;
+        }
+        catch (NullReferenceException)
+        {
+            Destroy(gameObject);
+        }
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 direction = corePosition - transform.position;
+        rb.AddForce(direction.normalized * speed, ForceMode2D.Force);
+        rb.velocity = rb.velocity.magnitude > speed ? rb.velocity.normalized * speed : rb.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Core"))
+        {
+            collision.gameObject.GetComponent<CoreBehaviour>().Damage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PC Master Client/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public float spawnTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            // Choose a random position off screen
+            float fixedValue = Random.value > 0.5f ? -0.2f : 1.2f;
+            float randomValue = Random.value;
+
+            Vector3 screenPoint;
+
+            if (Random.value > 0.5f)
+            {
+                screenPoint = new Vector3(fixedValue, randomValue, 10);
+            }
+            else
+            {
+                screenPoint = new Vector3(randomValue, fixedValue, 10);
+            }
+            
+            Vector3 worldPoint = Camera.main.ViewportToWorldPoint(screenPoint);
+            GameObject newEnemy = (GameObject)Instantiate(enemy, worldPoint, Quaternion.identity);
+            newEnemy.transform.parent = transform;
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
