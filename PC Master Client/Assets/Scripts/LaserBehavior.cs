d126612e3343866cd9f690d864c35d21016db752
Parker Greene <pwgreene@mit.edu>
2016-04-03 18:53:53 -0400

    added a player spaceship with shooting behavior

---
+++ PC Master Client/Assets/Scripts/LaserBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBehavior : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public int speed;
+	public GameObject owner;
+	public int life;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddRelativeForce (new Vector2(0, speed));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (life == 0) {
+			Destroy (gameObject);
+		} else {
+			life--;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Enemy"))
+		{
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ PC Master Client/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int speed;
+	public float rotationSpeed;
+	public int fireRate;
+
+	float playerVertical;
+	float playerHorizontal;
+	bool playerFire;
+
+	Rigidbody2D rb;
+
+	int timeElapsedSinceFire;
+
+	public GameObject laser;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		playerVertical = 0;
+		playerHorizontal = 0;
+		timeElapsedSinceFire = 0;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		playerHorizontal = Input.GetAxis("Horizontal");
+		playerVertical = Input.GetAxis("Vertical");
+		playerFire = Input.GetButtonDown("Fire1");
+		if (timeElapsedSinceFire < fireRate) 
+		{
+			timeElapsedSinceFire++;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddRelativeForce (new Vector2 (0, playerVertical * speed));
+		rb.AddTorque (-playerHorizontal * rotationSpeed);
+		if (playerFire) {
+			Fire ();
+		}
+		print (timeElapsedSinceFire);
+	}
+
+	void Fire()
+	{
+		print ("fire");
+		if (timeElapsedSinceFire == fireRate) {
+			Instantiate (laser, transform.position, transform.rotation);
+			timeElapsedSinceFire = 0;
+		}
+	}
+}
