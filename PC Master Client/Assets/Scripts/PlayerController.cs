33920a8b3be5d2bf1fbd08e8635e9ffba005b7d9
Parker Greene <pwgreene@mit.edu>
2016-04-03 20:13:53 -0400

    player now shoots two lasers and they are the same color as their ship

--- PC Master Client/Assets/Scripts/PlayerController.cs
+++ PC Master Client/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour {
 	int timeElapsedSinceFire;
 
 	public GameObject laser;
+	public Color color;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +24,11 @@ public class PlayerController : MonoBehaviour {
 		playerVertical = 0;
 		playerHorizontal = 0;
 		timeElapsedSinceFire = 0;
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+		if (sprite != null) {
+			color = sprite.color;
+		}
+		print (color);
 	}
 
 	// Update is called once per frame
@@ -51,7 +57,15 @@ public class PlayerController : MonoBehaviour {
 	{
 		print ("fire");
 		if (timeElapsedSinceFire == fireRate) {
-			Instantiate (laser, transform.position, transform.rotation);
+			//create two new lasers to fire and set them equal to the color of the parent
+			Vector3 rightBarrelEnd = transform.position +
+				(Quaternion.Euler (transform.eulerAngles) * new Vector3 (.4f, 0, 0));
+			Vector3 leftBarrelEnd = transform.position +
+				(Quaternion.Euler (transform.eulerAngles) * new Vector3 (-.4f, 0, 0));
+			GameObject rightLaser = Instantiate (laser, rightBarrelEnd, transform.rotation) as GameObject;
+			rightLaser.GetComponent<SpriteRenderer> ().color = color;
+			GameObject leftLaser = Instantiate (laser, leftBarrelEnd, transform.rotation) as GameObject;
+			leftLaser.GetComponent<SpriteRenderer> ().color = color;
 			timeElapsedSinceFire = 0;
 		}
 	}
