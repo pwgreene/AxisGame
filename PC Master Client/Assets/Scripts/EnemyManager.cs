43f1d1aa13912d1708aea8442130132dfc2652e0
Parker Greene <pwgreene@mit.edu>
2016-04-05 14:52:37 -0400

    added a warning sign to indicate which side enemies are coming from

--- PC Master Client/Assets/Scripts/EnemyManager.cs
+++ PC Master Client/Assets/Scripts/EnemyManager.cs
@@ -5,6 +5,7 @@ public class EnemyManager : MonoBehaviour
 {
 
     public GameObject enemy;
+	public GameObject enemyWarning;
     public float spawnTime;
 
     // Use this for initialization
@@ -21,19 +22,32 @@ public class EnemyManager : MonoBehaviour
             float fixedValue = Random.value > 0.5f ? -0.2f : 1.2f;
             float randomValue = Random.value;
 
-            Vector3 screenPoint;
+            Vector3 enemyScreenPoint;
+			Vector3 warningScreenPoint;
 
             if (Random.value > 0.5f)
             {
-                screenPoint = new Vector3(fixedValue, randomValue, 10);
+                enemyScreenPoint = new Vector3(fixedValue, randomValue, 10);
+				if (fixedValue >= 0) { //spawing point is on right of screen
+					warningScreenPoint = new Vector3 (.9f, .5f, 10);
+				} else {
+					warningScreenPoint = new Vector3 (.1f, .5f, 10);
+				}
             }
             else
             {
-                screenPoint = new Vector3(randomValue, fixedValue, 10);
+                enemyScreenPoint = new Vector3(randomValue, fixedValue, 10);
+				if (fixedValue >= 0) { //spawing point is on top of screen
+					warningScreenPoint = new Vector3 (.5f, .9f, 10);
+				} else {
+					warningScreenPoint = new Vector3 (.5f, .1f, 10);
+				}
             }
             
-            Vector3 worldPoint = Camera.main.ViewportToWorldPoint(screenPoint);
-            GameObject newEnemy = (GameObject)Instantiate(enemy, worldPoint, Quaternion.identity);
+            Vector3 enemyWorldPoint = Camera.main.ViewportToWorldPoint(enemyScreenPoint);
+			Vector3 warningWorldPoint = Camera.main.ViewportToWorldPoint (warningScreenPoint);
+			Instantiate (enemyWarning, warningWorldPoint, Quaternion.identity);
+            GameObject newEnemy = (GameObject)Instantiate(enemy, enemyWorldPoint, Quaternion.identity);
             newEnemy.transform.parent = transform;
             yield return new WaitForSeconds(spawnTime);
         }
---
+++ PC Master Client/Assets/Scripts/WarningBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WarningBehavior : MonoBehaviour {
+
+	public int lifespan;
+	int life;
+
+	public Vector3 corePosition;
+
+	// Use this for initialization
+	void Start () {
+		life = 0;
+		try
+		{
+			corePosition = GameObject.FindGameObjectWithTag("Core").transform.position;
+		}
+		catch (NullReferenceException)
+		{
+			Destroy(gameObject);
+		}
+		//point towards core and move onto screen
+		Vector3 direction = corePosition - transform.position;
+		float angle = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		life++;
+		if (life >= lifespan) {
+			Destroy (gameObject);
+		}
+	}
+}
