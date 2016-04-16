4fc5b3d0d55ff6c2f053e52398abdf8431023498
Roosevelt <rboyland@mit.edu>
2016-04-16 03:48:16 -0400

    Created a homing missile, and starting on a framework for weapon generation

---
+++ PC Master Client/Assets/Scripts/WeaponScripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Weapon {
+
+	void Fire ();
+
+	int getFireRate ();
+}
+
+public interface Projectile{
+
+	int getDamage ();
+
+	int getLife (); 
+}
