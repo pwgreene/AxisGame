dec65ed0a848e78687b425e02433f213ae73d2ae
Parker Greene <pwgreene@mit.edu>
2016-05-02 18:04:03 -0400

    added particle effects to enemy deaths

---
+++ PC Master Client/Assets/EnemyExplosionBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyExplosionBehaviour : MonoBehaviour {
+
+	public int lifetime;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("SelfDestruct", lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SelfDestruct() {
+		Destroy (gameObject);
+	}
+}
